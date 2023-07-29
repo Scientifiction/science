9d03718489a07253894c229840b58b2b24fba738
Sife-shuo <88375951+Sife-shuo@users.noreply.github.com>
2023-07-29 20:44:51 +0800

    Arrange Files

--- Chemistry/js/nodejs/test/test.js
+++ Chemistry/js/nodejs/test/test.js
@@ -1,6 +1,6 @@
 const {Equation}=require("../index");
 
-var a=new Equation("Ca(OH)2+HCl=CaCl2+H2O");
+var a=new Equation("Cu3P+CuSO4+H2O=Cu+H2SO4+H3PO4");
 console.log(a+"");
 a.trim();
 console.log(a+"");
