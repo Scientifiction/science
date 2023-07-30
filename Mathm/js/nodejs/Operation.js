38d7dc80b0c0d6ad4ce9a366a57d19c1d29d4147
Sife-shuo <88375951+Sife-shuo@users.noreply.github.com>
2023-07-30 08:45:26 +0800

    Add Mathm.Operation

---
+++ Mathm/js/nodejs/Operation.js
@@ -0,0 +1,42 @@
+var Operation={};
+Operation.list=["I","Matrix"];
+Operation.add=function(a,b){
+    if(a.type=="I"||a.type=="Matrix"){
+        if(b.type&&Operation.list.indexOf(b.type)>Operation.list.indexOf(a.type)){
+            return b.add(a)
+        }else{
+            return a.add(b);
+        }
+    }else if(b.type=="I"||b.type=="Matrix"){
+        return b.add(a);
+    }else{
+        return a+b;
+    }
+}
+Operation.reduce=function(a,b){
+    if(a.type=="I"||a.type=="Matrix"){
+        if(b.type&&Operation.list.indexOf(b.type)>Operation.list.indexOf(a.type)){
+            return b.reduce(a)
+        }else{
+            return a.reduce(b);
+        }
+    }else if(b.type=="I"||b.type=="Matrix"){
+        return b.reduce(a);
+    }else{
+        return a-b;
+    }
+}
+Operation.mult=function(a,b){
+    if(a.type=="I"||a.type=="Matrix"){
+        if(b.type&&Operation.list.indexOf(b.type)>Operation.list.indexOf(a.type)){
+            return b.mult(a)
+        }else{
+            return a.mult(b);
+        }
+    }else if(b.type=="I"||b.type=="Matrix"){
+        return b.mult(a);
+    }else{
+        return a*b;
+    }
+}
+module.exports=Operation;
