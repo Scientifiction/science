0a3c8483b7eaeb4da116341dafe7e1b8b9ddba2b
Sife-shuo <88375951+Sife-shuo@users.noreply.github.com>
2023-08-02 17:32:20 +0800

    Rename Mathm.Vector to Vector2d

---
+++ js/Mathm/nodejs/Vector2d.js
@@ -0,0 +1,37 @@
+const Operation=require("./Operation");
+class Vector2d{
+    constructor(a,b){
+        this.a=a;
+        this.b=b;
+        this.type="Vector2d";
+    }
+    add(n){
+        if(n.type=="Vector2d"){
+            return new Vector2d(this.a+n.a,this.b+n.b);
+        }else{
+            throw("Vectors can only be added to vectors")
+        }
+    }
+    reduce(n){
+        if(n.type=="Vector2d"){
+            return new Vector2d(this.a-n.a,this.b-n.b);
+        }else{
+            throw("Vectors can only be reduced to vectors")
+        }
+    }
+    mult(n){
+        if(n.type=="Vector2d"){
+            return new Vector2d(Operation.mult(this.a,n.a),Operation.mult(this.b,n.b));
+        }else{
+            return new Vector2d(Operation.mult(this.a,n),Operation.mult(this.b,n));
+        }
+    }
+    divide(n){
+        if(n.type=="Vector2d"){
+            return new Vector2d(Operation.divide(this.a,n.a),Operation.divide(this.b,n.b));
+        }else{
+            return new Vector2d(Operation.divide(this.a,n),Operation.divide(this.b,n));
+        }
+    }
+}
+module.exports=Vector2d;
