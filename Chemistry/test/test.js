bb1a09b80e1d0e065a00ecb3fb3d5f918f8f2355
Sife-shuo <88375951+Sife-shuo@users.noreply.github.com>
2023-08-07 14:39:17 +0800

    Improve test

--- Chemistry/test/test.js
+++ Chemistry/test/test.js
@@ -1,6 +1,8 @@
 const Chemistry=require("../Chemistry");
 
-var a=new Chemistry.Equation("H2+O2=H2O");
+//var a=new Chemistry.Equation("CH3CHO+Cu(OH)2+NaOH=CH3COONa+CuO2+H2O");
+var a=new Chemistry.Equation("O2+H2=H2O");
 console.log(a+"");
-a.trim();
+console.log(a.trim());
 console.log(a+"");
+console.log(a)
